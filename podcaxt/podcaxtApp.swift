13ec28a4ae446d3ca8669797d3eda80a38e93c88
Rafael Melo <rafaelnunesmelo@gmail.com>
2026-03-26 15:21:49 -0300

    chore: update .gitignore and add iOS project structure to root
    - Add Xcode project files (project.pbxproj, xcworkspace) to root
    - Add SwiftUI source files (podcaxtApp.swift, ContentView.swift)
    - Add Assets.xcassets with AppIcon and AccentColor
    - Add unit test and UI test boilerplate targets

---
+++ podcaxt/podcaxtApp.swift
@@ -0,0 +1,17 @@
+//
+//  podcaxtApp.swift
+//  podcaxt
+//
+//  Created by Rafael Nunes on 26/03/26.
+//
+
+import SwiftUI
+
+@main
+struct podcaxtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
